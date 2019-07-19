2830d16fa6bb48a0cffb5e9887a380c8cd92a317
Saravanan P <saravananp@cloudiq.io>
2019-07-19 17:21:31 +0530

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,15 @@
+docker build -t saravp123/multi-client:latest -t saravp123/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t saravp123/multi-server:latest -t saravp123/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t saravp123/multi-worker:latest -t saravp123/multi-worker:$SHA -f ./server/Dockerfile ./worker
+docker push saravp123/multi-client:latest
+docker push saravp123/multi-server:latest
+docker push saravp123/multi-worker:latest
+
+docker push saravp123/multi-client:$SHA
+docker push saravp123/multi-server:$SHA
+docker push saravp123/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/server-deployment server=saravp123/multi-server:$SHA
+kubectl set image deployments/client-deployment client=saravp123/multi-client:$SHA
+kubectl set image deployments/client-deployment worker=saravp123/multi-worker:$SHA
