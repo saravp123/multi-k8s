cf77ce01afae5d72fe5124deff3f3ceeca31f533
Saravanan P <saravananp@cloudiq.io>
2019-07-19 17:44:04 +0530

    fixed deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 docker build -t saravp123/multi-client:latest -t saravp123/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t saravp123/multi-server:latest -t saravp123/multi-server:$SHA -f ./server/Dockerfile ./server
-docker build -t saravp123/multi-worker:latest -t saravp123/multi-worker:$SHA -f ./server/Dockerfile ./worker
+docker build -t saravp123/multi-worker:latest -t saravp123/multi-worker:$SHA -f ./worker/Dockerfile ./worker
 docker push saravp123/multi-client:latest
 docker push saravp123/multi-server:latest
 docker push saravp123/multi-worker:latest
